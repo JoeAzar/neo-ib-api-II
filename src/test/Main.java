24f124c18c594b1e82b07d51d6fcb2c129f7dffd
Joe Azar <joeazar@Macs-MacBook-Pro.local>
2017-07-02 15:29:46 -0400

    updated Main

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,28 +1,25 @@
 package test;
 
+import java.io.FileNotFoundException;
+
 import ch.aonyx.broker.ib.api.CallbackObject;
 import ch.aonyx.broker.ib.api.ClientCallback;
 import ch.aonyx.broker.ib.api.NeoIbApiClient;
 import ch.aonyx.broker.ib.api.NeoIbApiClientException;
 import ch.aonyx.broker.ib.api.Session;
 import ch.aonyx.broker.ib.api.contract.Contract;
-import ch.aonyx.broker.ib.api.contract.ContractSpecificationEvent;
-import ch.aonyx.broker.ib.api.contract.ContractSpecificationEventListener;
 import ch.aonyx.broker.ib.api.contract.ContractSpecificationRequest;
 import ch.aonyx.broker.ib.api.contract.SecurityType;
 import ch.aonyx.broker.ib.api.data.CompositeTickEvent;
 import ch.aonyx.broker.ib.api.data.CompositeTickEventListener;
 import ch.aonyx.broker.ib.api.data.MarketDataSubscriptionRequest;
-import ch.aonyx.broker.ib.api.data.TickGenericEvent;
-import ch.aonyx.broker.ib.api.data.TickGenericEventListener;
-import ch.aonyx.broker.ib.api.data.TickSizeEvent;
-import ch.aonyx.broker.ib.api.data.TickSizeEventListener;
 import ch.aonyx.broker.ib.api.net.ConnectionCallback;
 import ch.aonyx.broker.ib.api.net.ConnectionException;
 import ch.aonyx.broker.ib.api.net.ConnectionParameters;
 
 public class Main {
-	public static void main(final String[] args) {
+	public static void main(final String[] args) throws FileNotFoundException {
+		//System.setOut(new PrintStream(new File("test.txt")));
 		new Main();
 	}
 
@@ -31,10 +28,6 @@ public class Main {
 		apiClient.connect(new ConnectionParameters(1), new ConnectionCallback() {
 			@Override
 			public void onSuccess(final Session session) {
-				System.out.println("something");
-				session.registerListener(new MyContractSpecificationEventListener());
-				session.registerListener(new MyTickSizeEventListener());
-				session.registerListener(new MyTickGenericEventListener());
 				session.registerListener(new MyCompositeTickEventListener());
 
 				Contract contract = getContract("SPY");
@@ -42,13 +35,14 @@ public class Main {
 				session.subscribe(new MarketDataSubscriptionRequest(contract));
 
 				session.start();
-				System.out.println("started");
 			}
 
 			private Contract getContract(final String symbol) {
 				final Contract contract = new Contract();
 				contract.setSecurityType(SecurityType.STOCK);
 				contract.setSymbol(symbol);
+				contract.setExchange("SMART");
+				contract.setCurrencyCode("USD");
 				return contract;
 			}
 
@@ -59,50 +53,29 @@ public class Main {
 		});
 	}
 }
+
 class MyClientCallback implements ClientCallback {
 
 	@Override
 	public void onSuccess(CallbackObject object) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("THIS: " + object);
+
 	}
 
 	@Override
 	public void onFailure(NeoIbApiClientException exception) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
 
-class MyContractSpecificationEventListener implements ContractSpecificationEventListener {
 
-	@Override
-	public void notify(ContractSpecificationEvent event) {
-		System.out.println("here1");
-		
-	}
-}
-class MyTickSizeEventListener implements TickSizeEventListener {
-
-	@Override
-	public void notify(TickSizeEvent event) {
-		// TODO Auto-generated method stub
-		System.out.println("here2");
-	}
-}
-class MyTickGenericEventListener implements TickGenericEventListener {
-
-	@Override
-	public void notify(TickGenericEvent event) {
-		// TODO Auto-generated method stub
-		System.out.println("here3");
-	}
-}
 class MyCompositeTickEventListener implements CompositeTickEventListener {
 
 	@Override
 	public void notify(CompositeTickEvent event) {
 		// TODO Auto-generated method stub
+		System.out.println(event.getTickPriceEvent());
 		System.out.println("here4");
 	}
 }
